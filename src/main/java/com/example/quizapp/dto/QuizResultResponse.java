a0832f1d4c9279ef91990f73960b2d9807dc1d22
Suvarna LNU <suvarna.lnu@i-exceed.com>
2026-08-26 18:22:31 +0530

    Initial commit

---
+++ src/main/java/com/example/quizapp/dto/QuizResultResponse.java
@@ -0,0 +1,21 @@
+package com.example.quizapp.dto;
+
+import com.example.quizapp.entity.AttemptStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuizResultResponse {
+
+    private Long quizId;
+    private Integer score;
+    private Integer totalMarks;
+    private Double percentage;
+    private AttemptStatus status;
+    private LocalDateTime submittedAt;
+}
